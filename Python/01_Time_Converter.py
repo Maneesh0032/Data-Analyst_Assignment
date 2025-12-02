67e0de6fa7490281a3d8278fac49228f60b50e6d
Maneesh0032 <126312620+Maneesh0032@users.noreply.github.com>
2025-12-02 21:20:08 +0530

    Update 01_Time_Converter.py

--- Python/01_Time_Converter.py
+++ Python/01_Time_Converter.py
@@ -1,4 +1,6 @@
-m = int(input("Enter minutes: "))
-hrs = m // 60
-mins = m % 60
-print(f"{hrs} hrs {mins} minutes")
+def convert_minutes(minutes):
+    hours = minutes // 60
+    remaining_minutes = minutes % 60
+    return f"{hours} hr{'s' if hours != 1 else ''} {remaining_minutes} minute{'s' if remaining_minutes != 1 else ''}"
+
+print(convert_minutes(int(input("input : "))))
